b3bf02cb8996a21f46ae04b03d7c3834bf0be2d4
Jens John <dev@2ion.de>
2017-08-29 00:50:39 +0200

    main: split TUI functions

--- src/oxfs.c
+++ src/oxfs.c
@@ -10,11 +10,80 @@
 
 #include "oxfs.h"
 
-static void usage(void);
+static void ox_usage(void);
+static void ox_cmdline(int, char**, OXState*, OXParams*);
 
-void usage(void)
+void ox_usage(void)
 {
   puts("Usage: oxfs [-h] [-H pinentry-hook] url mount-point");
+}
+
+void ox_cmdline(int argc, char **argv, OXState *st, OXParams *pm)
+{
+  int opt;
+
+  while ((opt = getopt(argc, argv, "H:h")) != -1)
+  {
+    switch (opt)
+    {
+      case 'h':
+        ox_usage();
+        exit(EXIT_SUCCESS);
+      case 'H':
+        pm->pinentry_hook = talloc_strdup(st->root_ctx, optarg);
+        pm->pinentry_mode = OX_PINENTRY_HOOK;
+        break;
+      case '?':
+        ox_usage();
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  if (argc - optind < 2) {
+    LERROR(EXIT_FAILURE, 0, "Missing URL and/or mount point");
+  }
+
+  pm->url = talloc_strdup(st->root_ctx, argv[optind++]);
+  pm->mountpoint = talloc_strdup(st->root_ctx, argv[optind]);
+
+  /* Process pinentry hook */
+
+  switch (pm->pinentry_mode)
+  {
+    case OX_PINENTRY_READLNE:
+      {
+        char *p;
+
+        if(p = readline("User> ")) {
+          pm->username = talloc_strdup(st->root_ctx, (const char*)p);
+          free(p);
+        }
+
+        if(p = readline("Password> ")) {
+          pm->password = talloc_strdup(st->root_ctx, (const char*)p);
+          free(p);
+        }
+      }
+      break;
+    case OX_PINENTRY_HOOK:
+      if(strcmp(pm->pinentry_hook, "@ENV") == 0) {
+        {
+          char *p;
+          if(p = getenv("OXFS_USERNAME"))
+            pm->username = talloc_strdup(st->root_ctx, (const char*)p);
+          else
+            LERROR(0, 0, "warning: OXFS_USERNAME is empty");
+
+          if(p = getenv("OXFS_PASSWORD"))
+            pm->password = talloc_strdup(st->root_ctx, (const char*)p);
+          else
+            LERROR(0, 0, "warning: OXFS_PASSWORD is empty");
+        }
+      } else {
+        /* Run hooker */
+      }
+      break;
+  }
 }
 
 int main(int argc, char** argv)
@@ -28,69 +97,11 @@ int main(int argc, char** argv)
     .url = NULL,
     .username = NULL,
   };
-  int opt;
-
-  /* Memory tree */
 
   state.root_ctx = talloc_autofree_context();
   GUARD_NULL(state.root_ctx);
 
-  /* CLI parser */
-
-  while ((opt = getopt(argc, argv, "H:h")) != -1)
-  {
-    switch (opt)
-    {
-      case 'h':
-        usage();
-        return EXIT_SUCCESS;
-      case 'H':
-        params.pinentry_hook = talloc_strdup(state.root_ctx, optarg);
-        params.pinentry_mode = OX_PINENTRY_HOOK;
-        break;
-    }
-  }
-
-  if (argc - optind < 2) {
-    LERROR(EXIT_FAILURE, 0, "Missing URL and/or mount point");
-  }
-
-  params.url = talloc_strdup(state.root_ctx, argv[optind++]);
-  params.mountpoint = talloc_strdup(state.root_ctx, argv[optind]);
-
-  /* Process pinentry hook */
-
-  switch (params.pinentry_mode)
-  {
-    case OX_PINENTRY_READLNE:
-      {
-        char *p;
-
-        if(p = readline("User> ")) {
-          params.username = talloc_strdup(state.root_ctx, (const char*)p);
-          free(p);
-        }
-
-        if(p = readline("Password> ")) {
-          params.password = talloc_strdup(state.root_ctx, (const char*)p);
-          free(p);
-        }
-      }
-      break;
-    case OX_PINENTRY_HOOK:
-      if(strcmp(params.pinentry_hook, "@ENV") == 0) {
-        {
-          char *p;
-          if(p = getenv("OXFS_USERNAME"))
-            params.username = talloc_strdup(state.root_ctx, (const char*)p);
-          if(p = getenv("OXFS_PASSWORD"))
-            params.password = talloc_strdup(state.root_ctx, (const char*)p);
-        }
-      } else {
-        /* execute hooker and read stdout */
-      }
-      break;
-  }
-
+  ox_cmdline(argc, argv, &state, &params);
+  
   return EXIT_SUCCESS;
 }
